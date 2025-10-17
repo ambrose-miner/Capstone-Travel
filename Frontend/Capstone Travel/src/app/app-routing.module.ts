a4cb35ed281943115bf20679564965b4a9b0df46
ambrose-miner <ambroseminer@yahoo.com>
2025-10-17 16:59:25 -0700

    10/17 push

--- Frontend/Capstone Travel/src/app/app-routing.module.ts
+++ Frontend/Capstone Travel/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginpageComponent } from './loginpage/loginpage.component';
-
+import { FlightBookingComponent } from './flight-booking/flight-booking.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 const routes: Routes = [
-  {path:"loginpage",component:LoginpageComponent},
+  {path:'',component:LoginpageComponent},
+  {path:"flight-booking",component:FlightBookingComponent},
+  {path:"signuppage",component:SignuppageComponent},
+  {path:"user-dashboard",component:UserDashboardComponent},
+  {path:"admin-dashboard",component:AdminDashboardComponent},
+  {path:'**' , redirectTo: ''}
+
 ];
 
 @NgModule({
